1bc0bd7dbde38c30cdca8df3185857e1c62da8b4
John Doe <johndoe@example.com>
2021-09-04 20:41:45 +0800

    x

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="wedding-editor" ref="editor">
-    <header class="editor-header">
-        <a href="javascript:;"></a>
-        <a href="javascript:;" class="minimum"></a>
-        <a href="javascript:;" class="maximum"></a>
-    </header>
-    <!-- 日期 -->
-    <p class="code">Last login: <span>{{ startDate }}</span> on ttys001</p>
     <!--代码编辑区-->
     <pre>
       <code v-html="highlightedCode"></code>
@@ -17,107 +10,125 @@
       v-if="(canStart || hasClosed) && !canOpen"
       @click="canOpen = true"
     >
-      <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+      <svg
+        viewBox="0 0 24 24"
+        width="18"
+        height="18"
+        stroke="currentColor"
+        stroke-width="2"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="css-i6dzq1"
+      >
         <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
         <line x1="9" y1="3" x2="9" y2="21"></line>
       </svg>
     </div>
-    <Executions :canExecute="canExecute" @onUpdating="scrollToBottom" @onFinish="canOpen = true"/>
-    <invitation :canOpen="canOpen" @onClose="canOpen = false, hasClosed = true" @sendBarrage="onAfterSending"/>
-    <Barrage :wish="wish" :canStart="canStart"/>
+    <Executions
+      :canExecute="canExecute"
+      @onUpdating="scrollToBottom"
+      @onFinish="canOpen = true"
+    />
+    <invitation
+      :canOpen="canOpen"
+      @onClose="(canOpen = false), (hasClosed = true)"
+      @sendBarrage="onAfterSending"
+    />
   </div>
 </template>
 
 <script>
-  import Prism from 'prismjs'
-  import 'prismjs/themes/prism-okaidia.css'
-  import '../utils/raf'
-  import data from '../mock/data'
+import Prism from "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+import "../utils/raf";
+import data from "../mock/data";
 
-  import Executions from './Executions'
-  import Invitation from './Invitation'
-  import Barrage from './Barrage'
+import Executions from "./Executions";
+import Invitation from "./Invitation";
 
-  export default {
-    name: 'Editor',
-    components: { Executions, Invitation, Barrage },
-    data() {
-      return {
-        startDate: '',
-        code: data.code,
-        currentCode: '',
-        isCursorVisible: 1,
-        canExecute: false,
-        canOpen: false,
-        wish: '',
-        hasClosed: false,
-        canStart: false
-      }
+export default {
+  name: "Editor",
+  components: { Executions, Invitation },
+  data() {
+    return {
+      startDate: "",
+      code: data.code,
+      currentCode: "",
+      isCursorVisible: 1,
+      canExecute: false,
+      canOpen: false,
+      wish: "",
+      hasClosed: false,
+      canStart: false,
+    };
+  },
+  created() {
+    this.startDate = new Date().toDateString();
+    this.progressivelyTyping();
+  },
+  updated() {
+    this.scrollToBottom();
+  },
+  computed: {
+    highlightedCode() {
+      const code = Prism.highlight(
+        this.currentCode,
+        Prism.languages.javascript
+      );
+      const codeWithCursor = `${code}<span style="opacity:${this.isCursorVisible}"> ▍</span>`;
+      return codeWithCursor;
     },
-    created() {
-      this.startDate = (new Date()).toDateString()
-      this.progressivelyTyping()
+  },
+  methods: {
+    scrollToBottom() {
+      // 保持页面一直滚到最下面
+      this.$refs.editor.scrollTop = 100000;
     },
-    updated() {
-      this.scrollToBottom()
-    },
-    computed: {
-      highlightedCode () {
-        const code = Prism.highlight(
-            this.currentCode,
-            Prism.languages.javascript
-          )
-        const codeWithCursor = `${code}<span style="opacity:${this.isCursorVisible}"> ▍</span>`
-        return codeWithCursor
-      }
-    },
-    methods: {
-      scrollToBottom() {
-        // 保持页面一直滚到最下面
-        this.$refs.editor.scrollTop = 100000
-      },
-      // 代码输入
-      progressivelyTyping() {
-        return new Promise((resolve) => {
-          let count = 0, typingCount = 0, typing
-          // 写代码每一帧的函数
-          let step = () => {
-            let randomNumber = Math.round(Math.random() * 6)
-            // 摸你打字的随机速度
-            if(count % 2 === 0 && randomNumber % 4 === 0){
-              this.currentCode = this.code.substring(0, typingCount)
-              typingCount++
-            }
-            // 大约每 24 帧光标闪动一次
-            if(count % 24 === 0){
-              this.isCursorVisible = this.isCursorVisible === 0 ? 1 : 0
-            }
-            count++
-            if (typingCount <= this.code.length) {
-              typing = requestAnimationFrame(step)
-            } else {
-              resolve()
-              this.canExecute = true
-              cancelAnimationFrame(typing)
-            }
+    // 代码输入
+    progressivelyTyping() {
+      return new Promise((resolve) => {
+        let count = 0,
+          typingCount = 0,
+          typing;
+        // 写代码每一帧的函数
+        let step = () => {
+          let randomNumber = Math.round(Math.random() * 6);
+          // 摸你打字的随机速度
+          if (count % 2 === 0 && randomNumber % 4 === 0) {
+            this.currentCode = this.code.substring(0, typingCount);
+            typingCount++;
           }
-          typing = requestAnimationFrame(step)
-        })
-      },
-      // 发送弹幕之后
-      onAfterSending(wish) {
-        this.wish = wish
-        this.canOpen = false
-        setTimeout(() => {
-          this.canStart = true
-        }, 800)
-      }
-    }
-  }
+          // 大约每 24 帧光标闪动一次
+          if (count % 24 === 0) {
+            this.isCursorVisible = this.isCursorVisible === 0 ? 1 : 0;
+          }
+          count++;
+          if (typingCount <= this.code.length) {
+            typing = requestAnimationFrame(step);
+          } else {
+            resolve();
+            this.canExecute = true;
+            cancelAnimationFrame(typing);
+          }
+        };
+        typing = requestAnimationFrame(step);
+      });
+    },
+    // 发送弹幕之后
+    onAfterSending(wish) {
+      this.wish = wish;
+      this.canOpen = false;
+      setTimeout(() => {
+        this.canStart = true;
+      }, 800);
+    },
+  },
+};
 </script>
 
 <style lang="less">
-.wedding-editor{
+.wedding-editor {
   position: absolute;
   top: 0;
   left: 0;
@@ -132,55 +143,55 @@
   -webkit-transform-origin: 0 0;
   transition: all 1.6s cubic-bezier(0.4, 0, 1, 1);
   -webkit-transition: all 1.6s cubic-bezier(0.4, 0, 1, 1);
-  .editor-header{
+  .editor-header {
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     padding: 12px;
     overflow: hidden;
-    background: #2B2B48;
+    background: #2b2b48;
     z-index: 3;
-    >a{
+    > a {
       float: left;
       display: block;
       width: 16px;
       height: 16px;
       margin-right: 5px;
       border-radius: 8px;
-      background: #FC615D;
-      &.minimum{
-        background: #FDBC40;
+      background: #fc615d;
+      &.minimum {
+        background: #fdbc40;
       }
-      &.maximum{
-        background: #34C84A;
+      &.maximum {
+        background: #34c84a;
       }
     }
   }
-  p.code{
+  p.code {
     margin: 0;
-    color: #BBB;
+    color: #bbb;
     line-height: 1.2;
-    font-family: 'Roboto Mono', 'Menlo', 'Monaco', Courier, monospace !important;
+    font-family: "Roboto Mono", "Menlo", "Monaco", Courier, monospace !important;
     font-weight: 500 !important;
-    font-size: 16px!important;
+    font-size: 16px !important;
   }
-  pre{
+  pre {
     margin: 0;
     white-space: pre-wrap;
-    code{
+    code {
       white-space: pre-wrap;
       word-break: break-all;
-      font-size: 16px!important;
+      font-size: 16px !important;
       margin: 0;
-      color: #BBB;
+      color: #bbb;
       line-height: 1.2;
-      font-family: 'Roboto Mono', 'Menlo', 'Monaco', Courier, monospace !important;
+      font-family: "Roboto Mono", "Menlo", "Monaco", Courier, monospace !important;
       font-weight: 500 !important;
       background: transparent;
     }
   }
-  .editor-open{
+  .editor-open {
     position: fixed;
     right: 20px;
     bottom: 20px;
@@ -192,7 +203,7 @@
     line-height: 18px;
     border: 5px solid #ffd69b;
     color: #a9895d;
-    background: #FFF1DE;
+    background: #fff1de;
     z-index: 1000;
   }
 }
--- src/mock/data.js
+++ src/mock/data.js
@@ -1,51 +1,8 @@
 export default {
-  code: 
-`
-// 今天心情好
-// 我给你表演写代码
-// 我要开始写啦
-const { 😆, 🤗 } = 🌎;
-😆.say('嫁给我吧!❤️');
-🤗.response('好的，只要你请我吃🍉！');
-Utils.marry(😆,🤗);
-Utils.generateInvitation(😆,🤗);
-// 好了我写完了
-// 我要开始运行啦`,
-  executions: [{
-    name:'初始化',
-    time: '',
-    duration: 0,
-    visible: false
-  }, {
-    name:'解压中',
-    time: '',
-    duration: 0,
-    visible: false
-  }, {
-    name:'组装中',
-    time: '',
-    duration: 0,
-    visible: false
-  }, {
-    name:'打包中',
-    time: '',
-    duration: 0,
-    visible: false
-  }, {
-    name:'微笑🙂',
-    time: '',
-    duration: 0,
-    visible: false
-  }, {
-    name:'大笑😄',
-    time: '',
-    duration: 0,
-    visible: false
-  }, {
-    name:'编译中...',
-    time: '',
-    visible: false
-  }],
+  code:
+    `
+// 今天心情好`,
+  executions: [],
   barrages: [
     '66666666',
     '愿你美梦成真',
